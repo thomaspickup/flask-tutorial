4e43863babcbd5f6212bced5ab94fbe14eee5383
Thomas Pickup <hello@thomaspickup.co.uk>
2019-05-02 17:33:24 +0100

    Added Base.html and block content

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-	return "<h1>Hi!</h1>"
+	user = {'username': 'Thomas'}
+	posts = [
+		{
+			'author' : {'username': 'John' },
+			'body' : 'Beautiful Day in Belfast'
+		},
+		{
+			'author' : {'username': 'Susan'},
+			'body': 'Avengers Endgame was so cool'
+		}
+		]
+	return render_template('index.html', title="Home", user=user, posts=posts)
